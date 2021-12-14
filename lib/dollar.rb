ac0969d97495c2d12fd2cc81f70fed6e13d73cdd
renatafreitas <68546902+renatafreitas@users.noreply.github.com>
2021-12-14 20:04:25 +0000

    multiplier initiates a new object

--- lib/dollar.rb
+++ lib/dollar.rb
@@ -4,12 +4,13 @@ class Dollar
 
   attr_reader :amount, :initial_amount
 
-  def initialize(initial_amount)
-    @initial_amount = initial_amount
+  def initialize(amount)
+    @amount = amount
   end
 
   def times(multiplier)
-    @amount = initial_amount * multiplier
+    new_amount = amount * multiplier
+    self.class.new(new_amount)
   end
 
 end
--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Dollar do
 
   it 'executes multiplication', :aggregate_failures do
     dollar = Dollar.new(5)
-    dollar.times(2)
-    expect(dollar.amount).to eq(10)
-    expect(dollar.initial_amount).to eq(5)
-    dollar.times(3)
-    expect(dollar.amount).to eq(15)
-    expect(dollar.initial_amount).to eq(5)
+    multiplied = dollar.times(2)
+    expect(multiplied.amount).to eq(10)
+    expect(dollar.amount).to eq(5)
+    multiplied = dollar.times(3)
+    expect(multiplied.amount).to eq(15)
+    expect(dollar.amount).to eq(5)
   end
 end
