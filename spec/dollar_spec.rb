1c31032a11e924796956ec9182071511396b52b5
renatafreitas <68546902+renatafreitas@users.noreply.github.com>
2021-12-14 19:21:01 +0000

    changed so initial value for dollar is not changed

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe Dollar do
     dollar = Dollar.new(5)
     dollar.times(2)
     expect(dollar.amount).to eq(10)
+    expect(dollar.initial_amount).to eq(5)
     dollar.times(3)
     expect(dollar.amount).to eq(15)
+    expect(dollar.initial_amount).to eq(5)
   end
 end
